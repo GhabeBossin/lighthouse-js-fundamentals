3589dc661f86e73fb052e52a484e44b966d77233
Gabrielle Bossin <gee.bossin@gmail.com>
2018-12-11 19:27:39 +0000

    Adds conditionals exersizes

---
+++ go-outside.js
@@ -0,0 +1,57 @@
+//First activity
+
+// var cold = false;
+
+// if (raining) {
+//   console.log("Don't forget your umbrella!");
+// }
+
+// if (cold) {
+//   console.log("Make sure you pick out a scarf!");
+// } else {
+//   console.log("Short sleeves are fine.")
+// }
+
+// console.log("Now you're ready to go outside!");
+
+//----
+
+//Second Activity
+
+var temp = -3;
+
+if (temp <0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temp <15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+console.log("Now you're ready to go outside!")
+
+
+//-----
+
+//Logical Operator Funtimes
+
+// var isCanine = false
+// var age = 2
+
+// if (isCanine && age < 2) {
+//   console.log("Has good pupper?");
+// } else if (isCanine && age >= 2) {
+//   console.log("Has good doggo?");
+// } else {
+//   console.log("Get a doggo!");
+// }
+
+// console.log("Then plz boop snoot.");
+
+
+
+
+
+
+
+
